80affe5a94109f20cedbb0d372822641cf7905fc
Sumit Jahagirdar <sumitrj99@gmail.com>
2020-10-30 10:48:46 +0530

    Update sd.py

--- sd.py
+++ sd.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import cv2 
 import imutils
 import numpy as np
+import os
 
 cap = cv2.VideoCapture('./example.mp4')
 
@@ -9,8 +10,6 @@ def dist(i,j):
     return ( (i[0]-j[0])**2 + (i[1]-j[1])**2 )**0.5
 
 def actuate(x,y):
-    
-    xbs = [200,400,600,800]
     
     if(0<=x<=200):
         signal(1)
@@ -24,19 +23,26 @@ def actuate(x,y):
     if(600<=x<=800):
         signal(4)
     
+def signal(x):
+    return 0
 
-def oneframe():
+def oneframe(ix):
+    
     ret,frame = cap.read()
-    plt.imshow(frame)
+    #plt.imshow(frame)
+    
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    
     image = frame
     (regions, _) = hog.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
+    
     hpts = [(i[0] + int(i[2]/2), i[1] + int(i[-1]/2) )  for i in regions]
     close = []
     far = []
     near = []
     dists = []
+    
     for i in range(len(hpts)):
         for j in range(len(hpts)):
             if(hpts[i]!=hpts[j]):
@@ -47,7 +53,9 @@ def oneframe():
                     near.append(i)
                 if(dist(hpts[i],hpts[j])>500):
                     far.append(i)
-    close = np.unique(close)
+    
+    close,near,far = np.unique(close),np.unique(near),np.unique(far)
+    
     for i in range(len(regions)):
         (x,y,w,h) = regions[i] 
         if(i in close):
@@ -61,4 +69,12 @@ def oneframe():
             continue
     plt.imshow(image)
     
-    return close, far, near, dists
+    cv2.imwrite('OI'+str(ix)+'.jpg',image)
+    
+    return {'Close':close, 'Far':far, 'Near':near, 'Dists':dists}
+    #return close, far, near, dists
+
+def run(itera,nf):
+    os.mkdir('Run'+str(itera))
+    for i in range(nf):
+        oneframe(i)
